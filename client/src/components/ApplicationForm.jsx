27b961eb9b8368a5164c1c4cc1b779cec1b82123
Chris Nii <chrisniiemperor@gmail.com>
2026-02-26 10:42:32 +0000

    Edit ApplicationForm.jsx

--- client/src/components/ApplicationForm.jsx
+++ client/src/components/ApplicationForm.jsx
@@ -28,7 +28,7 @@ const ApplicationForm = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/apply",
+        "https://youngdevs.onrender.com",
         formData
       );
 
